023f5555319628fa6f113c9f2b172729cbd9f869
nrlizahh <nurhalizah.sn101@gmail.com>
2025-09-15 14:40:08 +0700

    migrate database

---
+++ backend/src/db/migrate.js
@@ -0,0 +1,30 @@
+const pool = require('./config');
+
+const migration = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(225) NOT NULL
+      );
+    `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(200) NOT NULL,
+        description TEXT,
+        created_by INT REFERENCES users(id)
+      );
+    `);
+
+    console.log('Migration sukses 🚀');
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+migration();
