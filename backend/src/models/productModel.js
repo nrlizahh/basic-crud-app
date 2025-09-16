b621b6223ecbc1f06afcb0b45da3829262fe6927
nrlizahh <nurhalizah.sn101@gmail.com>
2025-09-16 15:39:49 +0700

    feat(backend) : add hardcoded image upload directly into database

--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -1,42 +1,61 @@
 const pool = require('../db/config');
 
-//get all products
+// All products with pagination
 async function getProducts(limit, offset) {
   const result = await pool.query(
-    'SELECT * FROM products ORDER BY id DESC LIMIT $1 OFFSET $2',
+    `SELECT * FROM products 
+     ORDER BY id DESC 
+     LIMIT $1 OFFSET $2`,
     [limit, offset]
   );
   return result.rows;
 }
 
-//create product
-async function createProduct(name, description, userId) {
+// Create product
+async function createProduct(name, description, imageUrl, userId) {
   const result = await pool.query(
-    'INSERT INTO products(name,description,user_id) VALUES($1,$2,$3) RETURNING *',
-    [name, description, userId]
+    `INSERT INTO products (name, description, image_url, user_id)
+     VALUES ($1, $2, $3, $4)
+     RETURNING *`,
+    [name, description, imageUrl, userId]
   );
   return result.rows[0];
 }
 
-//detail product
+// Detail product by ID
 async function getProductById(id) {
-  const result = await pool.query('SELECT * FROM products WHERE id=$1', [id]);
-  return result.rows[0];
-}
-
-//edit product
-async function editProduct(id, name, description) {
   const result = await pool.query(
-    'UPDATE products SET name=$1, description=$2 WHERE id=$3 RETURNING *',
-    [name, description, id]
+    `SELECT * FROM products WHERE id=$1`,
+    [id]
   );
   return result.rows[0];
 }
 
-//delete product
-async function deleteProduct(id) {
-  const result = await pool.query('DELETE FROM products WHERE id=$1', [id]);
-  return result.rowCount > 0;
+// Edit produk
+async function editProduct(id, name, description, imageUrl) {
+  const result = await pool.query(
+    `UPDATE products 
+     SET name=$1, description=$2, image_url=$3 
+     WHERE id=$4 
+     RETURNING *`,
+    [name, description, imageUrl, id]
+  );
+  return result.rows[0];
 }
 
-module.exports = {getProducts, createProduct, getProductById, editProduct, deleteProduct};
+// delete product
+async function deleteProduct(id) {
+  const result = await pool.query(
+    `DELETE FROM products WHERE id=$1`,
+    [id]
+  );
+  return result.rowCount > 0; 
+}
+
+module.exports = {
+  getProducts,
+  createProduct,
+  getProductById,
+  editProduct,
+  deleteProduct,
+};
