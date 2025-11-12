e0430e142984975a758750bde0db4e37c2bc3bb4
nrlizahh <nurhalizah.sn101@gmail.com>
2025-11-12 13:48:39 +0700

    deploy backend to server

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,17 +4,19 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const router = require("./routes/index");
+const cors = require("cors");
+
 const app = express();
 
-const cors = require("cors");
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+// Tes route
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
+
+module.exports = app;
